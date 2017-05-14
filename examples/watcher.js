050c6e44f04f6d4fe983aad105abb760f7e79e5b
Robert Eisele <robert@xarg.org>
2017-05-15 00:43:01 +0200

    Added watchdog

---
+++ examples/watcher.js
@@ -0,0 +1,15 @@
+
+var dhcp = require('../lib/dhcp.js');
+
+var s = dhcp.createBroadcastHandler();
+
+s.on('message', function (data) {
+
+  if (data.options[53] === dhcp.DHCPDISCOVER) {
+    if (data.chaddr === '12-34-56-78-90-AB') {
+      console.log("Welcome home!");
+    }
+  }
+});
+
+s.listen();
