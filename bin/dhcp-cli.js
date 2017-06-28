6c555fff5a8edfdd8733a620c569dfa9823ab059
Christopher Jacobs <cjacobs@drobo.com>
2017-06-28 11:01:12 -0700

    Fix indentation/cleanup.

--- bin/dhcp-cli.js
+++ bin/dhcp-cli.js
@@ -6,24 +6,23 @@ var path = require('path');
 var dhcp = require('../lib/dhcp.js');
 var argv = require('minimist')(process.argv.slice(2));
 
-
 // Create a client
 
 var client = dhcp.createClient({
-features: argv._
+  features: argv._
 });
 
 client.on('bound', function () {
 
-var opt = this._state.options;
+  var opt = this._state.options;
 
-// Print all requested options
-for (var i in opt) {
-  console.log(i, ": ", opt[i] instanceof Array ? opt[i].join(", ") : opt[i]);
-}
+  // Print all requested options
+  for (var i in opt) {
+    console.log(i, ": ", opt[i] instanceof Array ? opt[i].join(", ") : opt[i]);
+  }
 
-// Exit when finished
-process.exit();
+  // Exit when finished
+  process.exit();
 });
 
 client.listen();
